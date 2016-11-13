8133ad5a1910c534440f0dd3373c28096b3fb217
hakuna <hosikui.musyaana@gmail.com>
2016-11-13 12:14:57 +0900

    fragment追加

--- ShuuLiner/app/src/main/java/com/application/hakuna/shuuliner/ui/activity/InitActivity.java
+++ ShuuLiner/app/src/main/java/com/application/hakuna/shuuliner/ui/activity/InitActivity.java
@@ -1,7 +1,10 @@
 package com.application.hakuna.shuuliner;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.application.hakuna.shuuliner.ui.fragment.PartsFragment;
 
 public class InitActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class InitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
+        //fragmentの生成
+        PartsFragment fragment = new PartsFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, fragment);
+        transaction.commit();
     }
 }
